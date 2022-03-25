865ac7c98fa069c907ba3ce262a8eddc11d89d52
Roky7250 <sretko1sretko2@hotmail.com>
2022-03-25 08:23:11 +0100

    Updated diode handling in driver. Updated function names and description in driver.

--- kernel_module/mailbox_module.c
+++ kernel_module/mailbox_module.c
@@ -9,31 +9,44 @@
 #include <linux/uaccess.h> 
 #include <linux/gfp.h>
 #include <linux/string.h>
-#include <linux/gpio.h>
+#include <linux/gpio.h> //for device GPIO support
 
-#define PROJECT_BUFFSIZE 51
+#define mailbox_BUFFSIZE 51
 
+/* Struct used for storing messages inside driver */
 struct proj_data 
 {
-    char project_buff[PROJECT_BUFFSIZE];
+    char mailbox_buff[mailbox_BUFFSIZE];
     int lenght;
 };
 
-static struct gpio leds[] = {
-	{47, GPIOF_OUT_INIT_HIGH, "led0"},
-};
+/* Struct holdion diode data */
+static struct gpio diode = {47, GPIOF_OUT_INIT_HIGH, "led0"};
 
 /* Input parameter */
 int mode = 0; //default value encrypted
-module_param(mode, int, S_IWUSR | S_IRUSR); //alow user to write and read
+module_param(mode, int, S_IWUSR | S_IRUSR);
 
 /* Key used for encryption */
 static int key = 7;
 
-static struct proj_data project_data;
-static int project_count=1;
-static dev_t project_dev;
-static struct cdev project_cdev;
+/* Variables used for driver initialization */
+static struct proj_data mailbox_data;
+static int mailbox_count=1;
+static dev_t mailbox_dev;
+static struct cdev mailbox_cdev;
+
+/* Turns diode on */
+void turn_diode_on(void) 
+{
+	gpio_set_value(diode.gpio, 1);
+}
+
+/* Turns diode off */
+void turn_diode_off(void) 
+{
+	gpio_set_value(diode.gpio, 0);
+}
 
 /* Function for calulcation CRC of message */
 char calculate_crc(char* message, int lenght)
@@ -126,7 +139,7 @@ char* get_decrypted(char* encrypted, int length)
     return decrypted;
 }
 
-/* Appends CRC to encrypted message (decrypted or encrypted) */
+/* Appends CRC to encrypted message */
 char* encrypt_append_crc_length(char* message, int* length, char* crc)
 {
     char* encrypted, *message_crc;
@@ -156,18 +169,16 @@ char* encrypt_append_crc_length(char* message, int* length, char* crc)
     return message_crc;
 }
 
-int project_open (struct inode *pnode, struct file *pfile)
+/* Triggered when device file is opened */
+int mailbox_open (struct inode *pnode, struct file *pfile)
 {
-	int i;
     printk(KERN_DEBUG "%s called...\n", __FUNCTION__);
-	for(i = 0; i < ARRAY_SIZE(leds); i++) 
-    {
-		gpio_set_value(leds[i].gpio, 0);
-	}
+    turn_diode_off();
     return 0;
 }
 
-ssize_t project_read (struct file *pfile, char __user *buffer, size_t length, loff_t *offset)
+/* Triggered when data is read from device file */
+ssize_t mailbox_read (struct file *pfile, char __user *buffer, size_t length, loff_t *offset)
 {
     int msg_length = 0, data_size = 0;
     char crc;
@@ -177,7 +188,7 @@ ssize_t project_read (struct file *pfile, char __user *buffer, size_t length, lo
     /* If mode is encryption, then create message containing LENGHT, CRC and STRING */
     if (mode == 1)
     {
-        encrypted_crc = encrypt_append_crc_length(project_data.project_buff, &msg_length, &crc);
+        encrypted_crc = encrypt_append_crc_length(mailbox_data.mailbox_buff, &msg_length, &crc);
         if(encrypted_crc == NULL)
         {
             return -EFAULT;
@@ -204,12 +215,12 @@ ssize_t project_read (struct file *pfile, char __user *buffer, size_t length, lo
     else if (mode == 0) 
     { 
         /* If mode is decryption, then create message containing only STRING */
-        printk(KERN_DEBUG "driver [decrypted] = '%s'\n", project_data.project_buff);
-        if (copy_to_user(buffer, project_data.project_buff, project_data.lenght + 1) != 0)
+        printk(KERN_DEBUG "driver [decrypted] = '%s'\n", mailbox_data.mailbox_buff);
+        if (copy_to_user(buffer, mailbox_data.mailbox_buff, mailbox_data.lenght + 1) != 0)
         {
             return -EFAULT;
         } 
-        return project_data.lenght;
+        return mailbox_data.lenght;
     } 
     else 
     {
@@ -218,66 +229,60 @@ ssize_t project_read (struct file *pfile, char __user *buffer, size_t length, lo
     }
 }
 
-ssize_t project_write (struct file *pfile, const char __user *buffer, size_t length, loff_t *offset)
+/* Triggered when data is written to device file */
+ssize_t mailbox_write (struct file *pfile, const char __user *buffer, size_t length, loff_t *offset)
 {
     char* decrypted, *encrypted, msg_length, crc, dec_crc;
-    int i;
     printk(KERN_DEBUG "%s called...\n", __FUNCTION__);
 
     /* Check requested length */
     if (length > 50)
     {
-        printk(KERN_DEBUG "Requested write size '%d' exceeds allowed buffer driver size '%d'!\n", length, PROJECT_BUFFSIZE);
+        printk(KERN_DEBUG "Requested write size '%d' exceeds allowed buffer driver size '%d'!\n", length, mailbox_BUFFSIZE);
         return -EFAULT;
     }
     /* Reset memory. */
-    memset(project_data.project_buff, 0, PROJECT_BUFFSIZE);
+    memset(mailbox_data.mailbox_buff, 0, mailbox_BUFFSIZE);
 
     if (mode == 1)
     {
-        if (copy_from_user(project_data.project_buff, buffer, length))
+        if (copy_from_user(mailbox_data.mailbox_buff, buffer, length))
         {
             return -EFAULT;
         }
-        project_data.lenght = (int)length;
+        mailbox_data.lenght = (int)length;
         return length;
     }
     else if (mode == 0)
     {
         /* If mode is decryption, then create message containing only STRING */
-        if (copy_from_user(project_data.project_buff, buffer, length + 1))
+        if (copy_from_user(mailbox_data.mailbox_buff, buffer, length + 1))
         {
             return -EFAULT;
         }
-        msg_length = project_data.project_buff[0];
-        crc = project_data.project_buff[1];
-        encrypted = project_data.project_buff + 2;
+        msg_length = mailbox_data.mailbox_buff[0];
+        crc = mailbox_data.mailbox_buff[1];
+        encrypted = mailbox_data.mailbox_buff + 2;
         decrypted = get_decrypted(encrypted, msg_length);
         if (decrypted == NULL)
         {
             return -EFAULT;
         }
         dec_crc = calculate_crc(decrypted, msg_length);
-        memset(project_data.project_buff, 0, PROJECT_BUFFSIZE);
-        strncpy(project_data.project_buff, decrypted, msg_length + 1);
-        project_data.lenght = (int)msg_length + 1;
+        memset(mailbox_data.mailbox_buff, 0, mailbox_BUFFSIZE);
+        strncpy(mailbox_data.mailbox_buff, decrypted, msg_length + 1);
+        mailbox_data.lenght = (int)msg_length + 1;
 
         if (crc == dec_crc)
         {
             printk(KERN_DEBUG "CRC sum match");
-            for(i = 0; i < ARRAY_SIZE(leds); i++) 
-            {
-                gpio_set_value(leds[i].gpio, 1);
-            }
+            turn_diode_on();
         } 
         else
         {
             printk(KERN_DEBUG "CRC sum mismatch");
             printk(KERN_DEBUG "CRC sum match");
-            for(i = 0; i < ARRAY_SIZE(leds); i++) 
-            {
-                gpio_set_value(leds[i].gpio, 0);
-            }
+            turn_diode_off();
         }
         kfree(decrypted);
         return length;
@@ -289,7 +294,8 @@ ssize_t project_write (struct file *pfile, const char __user *buffer, size_t len
     }
 }
 
- long project_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
+/* Triggered when ioctl function is called from user space application */
+ long mailbox_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
  {
     int ret = 0;
     printk(KERN_DEBUG "%s called...\n", __FUNCTION__);
@@ -311,60 +317,59 @@ ssize_t project_write (struct file *pfile, const char __user *buffer, size_t len
     return ret;
 }
 
-int project_close (struct inode *pnode, struct file *pfile)
+/* Triggered when device file is closed */
+int mailbox_close (struct inode *pnode, struct file *pfile)
 {
     printk(KERN_DEBUG "%s called...\n", __FUNCTION__);      
     return 0;
 }
 
-struct file_operations project_file_operations =
-{
+/* Struct that defines device file operations and functions handling those operations */
+struct file_operations mailbox_file_operations = {
     .owner = THIS_MODULE,
-    .open = project_open,
-    .read = project_read,
-    .write = project_write,
-    .release = project_close,
-    .unlocked_ioctl = project_ioctl,
+    .open = mailbox_open,
+    .read = mailbox_read,
+    .write = mailbox_write,
+    .release = mailbox_close,
+    .unlocked_ioctl = mailbox_ioctl,
 };
 
-/* Add your code here */
-static int __init  project_init(void)
+/* Triggered when driver is insterted to kernel */
+static int __init  mailbox_init(void)
 {
     int err;
     printk(KERN_DEBUG "%s called...\n", __FUNCTION__);
-    if (alloc_chrdev_region(&project_dev, 0, project_count, "mailbox_module"))
+    if (alloc_chrdev_region(&mailbox_dev, 0, mailbox_count, "mailbox_module"))
     {
         err=-ENODEV;
         goto err_dev_unregister;
     }
-    cdev_init(&project_cdev, &project_file_operations);
-    if (cdev_add(&project_cdev, project_dev, project_count))
+    cdev_init(&mailbox_cdev, &mailbox_file_operations);
+    if (cdev_add(&mailbox_cdev, mailbox_dev, mailbox_count))
     {
         err=-ENODEV;
         goto err_dev_unregister;
     }
-    gpio_request_array(leds, ARRAY_SIZE(leds));
+    gpio_request_array(&diode, 1);
     return 0;
 
     err_dev_unregister:
-        unregister_chrdev_region(project_dev, project_count);
+        unregister_chrdev_region(mailbox_dev, mailbox_count);
         return err;
 }
 
-static void __exit project_exit(void)
+
+/* Triggered when driver is removed from kernel */
+static void __exit mailbox_exit(void)
 {
-    int i;
     printk(KERN_DEBUG "%s called...\n", __FUNCTION__);
-    cdev_del(&project_cdev);
-    unregister_chrdev_region(project_dev, project_count);
-	for(i = 0; i < ARRAY_SIZE(leds); i++) 
-    {
-		gpio_set_value(leds[i].gpio, 0);
-	}
+    cdev_del(&mailbox_cdev);
+    unregister_chrdev_region(mailbox_dev, mailbox_count);
+	turn_diode_off();
 }
 
-module_init(project_init);
-module_exit(project_exit);
+module_init(mailbox_init);
+module_exit(mailbox_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("This module is used for encrypting data using Caesar Cipher.");
